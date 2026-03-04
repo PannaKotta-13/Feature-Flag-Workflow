f06b27131e2b5bd262a8ee8c3b8f16933ebdb39c
Pannakotta <panissimo13@gmail.com>
2026-03-04 22:55:33 +0300

    develop 1.0 update

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,6 @@
 import kotlin.math.PI
 import kotlin.math.tan
 
-enum class Feature {
-    USE_CORRECTION_FACTOR,
-    USE_HIGH_PRECISION,
-    OUTPUT_IN_MICROTESLA
-}
-
-object FeatureFlags {
-
-    private val flags = mutableMapOf(
-        Feature.USE_CORRECTION_FACTOR to true,
-        Feature.USE_HIGH_PRECISION to true,
-        Feature.OUTPUT_IN_MICROTESLA to true
-    )
-
-    fun isEnabled(feature: Feature): Boolean {
-        return flags[feature] ?: false
-    }
-
-    fun set(feature: Feature, enabled: Boolean) {
-        flags[feature] = enabled
-    }
-}
-
 fun main() {
     //variables
     val n0 = 4 * PI * 1e-7
@@ -33,15 +10,7 @@ fun main() {
     val angle: DoubleArray = doubleArrayOf(29.0, 34.0, 39.0, 45.0, 50.0, 55.0, 60.0)
     //formula
     for ((i,a) in I.zip(angle)) {
-        val B_G = (n0 * i * N) / (2 * R * tan(Math.toRadians(a)))
-        if (FeatureFlags.isEnabled(Feature.USE_HIGH_PRECISION)) {
-            val correction = if (FeatureFlags.isEnabled(Feature.USE_CORRECTION_FACTOR)) 1.02 else 1.0
-            val B_G_new = correction * B_G
-            println(B_G_new)
-        } else if (FeatureFlags.isEnabled(Feature.OUTPUT_IN_MICROTESLA)) {
-            println(B_G * 1e6)
-        } else {
-            println("Ответ без feature flag: ${B_G}")
-        }
+        val B_G = 1.02 * (n0 * i * N) / (2 * R * tan(Math.toRadians(a)))
+        println(B_G * 1e6)
     }
 }
